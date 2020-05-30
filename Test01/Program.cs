2ed4034b38a731c3bd6905753b0c7fbeb82d5d55
Mary Ondra <vidra48@mail.ru>
2020-05-30 14:41:26 +0300

    Add project files.

---
+++ Test01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System; //импорт пространства имен
+
+namespace Test01
+{
+    class Program // объявление класса
+    {
+        static void Main(string[] args)  // объявление метода
+        {
+            int x = 12 * 30; // оператор 1
+            Console.WriteLine(x); // оператор 2
+
+            Console.WriteLine(FeetToInches(156540));
+            Console.WriteLine(FeetToInches(1500));
+            Console.WriteLine(FeetToInches(1125));
+
+            Console.WriteLine
+                (5 + 6 + 4 + 7 + 5 + 8 + 5 + 4 + 4 + 7 + 77 + 7 + 5 + 8);
+
+
+            Console.ReadLine();
+        } //конец метода
+
+        static int FeetToInches (int feet)
+        {
+            int inches = feet * 12;
+            return inches;
+        }
+
+
+    } // конец класса
+}
