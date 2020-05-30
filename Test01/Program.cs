935cfcfbf5ff53de0668600cc50958feaa2725dc
Mary Ondra <vidra48@mail.ru>
2020-05-30 15:06:41 +0300

    delete

--- Test01/Program.cs
+++ Test01/Program.cs
@@ -6,25 +6,14 @@ namespace Test01
     {
         static void Main(string[] args)  // объявление метода
         {
-            int x = 12 * 30; // оператор 1
-            Console.WriteLine(x); // оператор 2
-
-            Console.WriteLine(FeetToInches(156540));
-            Console.WriteLine(FeetToInches(1500));
-            Console.WriteLine(FeetToInches(1125));
-
-            Console.WriteLine
-                (5 + 6 + 4 + 7 + 5 + 8 + 5 + 4 + 4 + 7 + 77 + 7 + 5 + 8);
-
+            /* длинный комментарий который занимает 
+             несколько строк и не нужно постоянно 
+            проставлять знаки в начале строк*/
+            
+            // а этот нормально использовать для одной строки
 
             Console.ReadLine();
         } //конец метода
-
-        static int FeetToInches (int feet)
-        {
-            int inches = feet * 12;
-            return inches;
-        }
 
 
     } // конец класса
