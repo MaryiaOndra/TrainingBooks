1c50331043f57c7b3ac99179ab8c96f1e0607342
Mary Ondra <vidra48@mail.ru>
2020-05-30 16:39:52 +0300

    panda excersize

--- Test01/Program.cs
+++ Test01/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System; //импорт пространства имен
+using System.Security.Cryptography.X509Certificates;
 
 namespace Test01
 {
     class Program // объявление класса
     {
-        static void Main(string[] args)  // объявление метода
+        public class Panda
         {
-           
-            Console.ReadLine();
-        } //конец метода
+            public string Name;
+            public static int Population;
+            public Panda(string n)
+            {
+                Name = n;
+                Population = Population + 1;
+            }
+        }
 
+
+        static void Main(string[] args)// объявление метода
+        {
+            Panda p1 = new Panda("Панда По");
+            Panda p2 = new Panda("Панда Ру");
+
+            Console.WriteLine(p1.Name);
+            Console.WriteLine(p2.Name);
+
+            Console.WriteLine(Panda.Population);
+
+
+        }
+   
 
     } // конец класса
 }
