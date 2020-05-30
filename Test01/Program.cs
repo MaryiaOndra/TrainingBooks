0dbb4a85db0ba3eca4f998bfe0ed89635c1afe6f
Mary Ondra <vidra48@mail.ru>
2020-05-30 15:01:41 +0300

    bool example

--- Test01/Program.cs
+++ Test01/Program.cs
@@ -6,26 +6,17 @@ namespace Test01
     {
         static void Main(string[] args)  // объявление метода
         {
-            int x = 12 * 30; // оператор 1
-            Console.WriteLine(x); // оператор 2
 
-            Console.WriteLine(FeetToInches(156540));
-            Console.WriteLine(FeetToInches(1500));
-            Console.WriteLine(FeetToInches(1125));
+            bool simpleVar = false;
+            if (simpleVar)
+                Console.WriteLine("Это не будет выведено");
 
-            Console.WriteLine
-                (5 + 6 + 4 + 7 + 5 + 8 + 5 + 4 + 4 + 7 + 77 + 7 + 5 + 8);
-
-
-            Console.ReadLine();
+            int x = 6800;
+            bool lessThanAMile = x < 5280;
+            if (lessThanAMile)
+                Console.WriteLine("Это будет выведено " + x);
+ 
         } //конец метода
-
-        static int FeetToInches (int feet)
-        {
-            int inches = feet * 12;
-            return inches;
-        }
-
 
     } // конец класса
 }
