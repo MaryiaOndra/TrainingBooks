4b172a3d1d843d58afb521ac5b28ec3fcc8dd221
Mary Ondra <vidra48@mail.ru>
2020-05-30 14:55:30 +0300

    примеры строчек

--- Test01/Program.cs
+++ Test01/Program.cs
@@ -6,25 +6,19 @@ namespace Test01
     {
         static void Main(string[] args)  // объявление метода
         {
-            int x = 12 * 30; // оператор 1
-            Console.WriteLine(x); // оператор 2
+            string message = "Верни мне мой ";
+            string upperMessage = message.ToUpper();
+            Console.WriteLine(upperMessage);
 
-            Console.WriteLine(FeetToInches(156540));
-            Console.WriteLine(FeetToInches(1500));
-            Console.WriteLine(FeetToInches(1125));
-
-            Console.WriteLine
-                (5 + 6 + 4 + 7 + 5 + 8 + 5 + 4 + 4 + 7 + 77 + 7 + 5 + 8);
-
+            int x = 2007;
+            message = message + x;
+            Console.WriteLine(message);
 
             Console.ReadLine();
+
+       
         } //конец метода
 
-        static int FeetToInches (int feet)
-        {
-            int inches = feet * 12;
-            return inches;
-        }
 
 
     } // конец класса
