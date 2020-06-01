8ea6eff16c80afbc13c2bdac2d6e04e60e10bd95
Mary Ondra <vidra48@mail.ru>
2020-06-01 16:38:08 +0300

    new

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 1; i <= 10; i++){
+
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
